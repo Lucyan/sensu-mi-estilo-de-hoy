43426e4fb71e2c39bc281bec65a80ada4fb62602
Leonardo <leonardo311@gmail.com>
2014-01-08 02:00:07 -0300

    FIX: allow origin

--- API/index.php
+++ API/index.php
@@ -4,6 +4,7 @@ require 'config.php';
 
 date_default_timezone_set("America/Santiago");
 header('P3P: CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"');
+header('Access-Control-Allow-Origin: *');
 
 $app = new \Slim\Slim();
 
